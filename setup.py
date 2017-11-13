3393c129b4a646064ff058a5cbc207d545f67cd4
dead-beef <dead-beef@users.noreply.github.com>
2017-11-13 01:34:23 +0000

    update package data

--- setup.py
+++ setup.py
@@ -1,29 +1,52 @@
+#!/usr/bin/env python
+
+import os
 from unittest import TestLoader
 from setuptools import setup
 
 def tests():
     return TestLoader().discover('tests')
 
-setup(name='pygtkdrawingwindow',
-      version='0.1',
-      description='',
-      long_description='',
-      classifiers=[
-          'Programming Language :: Python :: 2',
-          'Programming Language :: Python :: 2.7',
-          'License :: OSI Approved :: MIT License'
-      ],
-      keywords='',
-      url='https://github.com/dead-beef/pygtkdrawingwindow',
-      author='dead-beef',
-      author_email='contact@dead-beef.tk',
-      license='MIT',
-      py_modules=['pygtkdrawingwindow'],
-      data_files=[
-          ('share/pygtkdrawingwindow',
-           ['README.md', 'LICENSE', 'demo.py'])
-      ],
-      test_suite='setup.tests',
-      install_requires=[],
-      include_package_data=True,
-      zip_safe=False)
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+try:
+    with open(os.path.join(BASE_DIR, 'README.rst')) as fp:
+        README = fp.read()
+except IOError:
+    README = ''
+
+setup(
+    name='pygtkdrawingwindow',
+    version='0.1.0',
+    description='',
+    long_description=README,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: X11 Applications :: GTK',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License'
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Multimedia :: Graphics',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Software Development :: Widget Sets'
+    ],
+    keywords='gtk',
+    url='https://github.com/dead-beef/pygtkdrawingwindow',
+    author='dead-beef',
+    license='MIT',
+    py_modules=['pygtkdrawingwindow'],
+    test_suite='setup.tests',
+    install_requires=[],
+    extras_require={
+        'dev': [
+            'sphinx',
+            'sphinx_rtd_theme',
+            'twine>=1.8.1',
+            'wheel'
+        ]
+    },
+    include_package_data=True,
+    zip_safe=False
+)
